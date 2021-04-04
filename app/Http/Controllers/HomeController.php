03a22e02b85e00f62ecef0eba3b3a4d7599bfa71
Mr.Programmer <m.mohammadi.developer@gmail.com>
2021-04-04 18:56:38 +0430

    62-one-to-many relationship

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,17 +3,46 @@
 namespace App\Http\Controllers;
 
 use App\Address;
+use App\Comment;
+use App\Post;
 use App\User;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
+
+    public function backup1()
+    {
+        // $address = Address::find(1);
+        // $user = User::find(1);
+        // dd($user->address);
+        // dd($address->user);
+    }
     
     public function index()
     {
-        $address = Address::find(1);
-        // $user = User::find(1);
-        // dd($user->address);
-        dd($address->user);
+        // $comment = Comment::find(1);
+        // $post = Post::find(1);
+        // $posts = Post::has('comments')->get();
+        // dd($comment->post);
+        // dd($post->comments);
+        // dd($posts);
+
+        // $comment = Comment::find(3);
+        // $comment->post()->associate(Post::first());
+        // dd($comment);
+        // $comment->save();
+
+        // $comment = Comment::find(3);
+        // $comment->post()->dessociate();
+        // dd($comment);
+        // $comment->save();
+
+        // $post = Post::find(1);
+        // dd($post->comments()->where('id', 2)->get());
+
+
+
+        
     }
 }

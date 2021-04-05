9b2af74b479a944cb1ae1737b9d7a33565722b7a
Mr.Programmer <m.mohammadi.developer@gmail.com>
2021-04-05 19:40:53 +0430

    66-67 many-to-many realationship

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,6 +6,7 @@ use App\Address;
 use App\Comment;
 use App\Country;
 use App\Post;
+use App\Tag;
 use App\User;
 use Illuminate\Http\Request;
 
@@ -42,11 +43,30 @@ class HomeController extends Controller
         $user = User::find(2);
         dd($user->carInfo);
 
+        $country = Country::find(2);
+        dd($country->posts);
+
     }
 
     public function index()
     {
-        $country = Country::find(2);
-        dd($country->posts);
+        // posts<->tags
+        // $post = Post::find(7);
+        // dd($post->tags);
+        // $tag = Tag::find(2);
+        // dd($tag->posts);
+
+        // $post = Post::find(3);
+        // $post->tags()->attach(2);
+        // $post->tags()->detach(2);
+        // $post->tags()->sync(1);
+        // $post->tags()->attach([1, 2]);
+
+        // $post = Post::find(12);
+        // dd($post->tags()->first()->pivot->costum_column);
+        // dd($post->tags()->first()->pivot->created_at);
+        // dd($post->tags()->first()->pivot->updated_at);
+
+
     }
 }
